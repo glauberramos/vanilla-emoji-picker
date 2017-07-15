650856575cb2f673b005e48b292459ca748f9f01
Glauber dutra ramos <glauberamos@gmail.com>
2017-07-15 18:50:46 -0300

    Generate dinamic elements

--- script.js
+++ script.js
@@ -1,9 +1,33 @@
 (() => {
   const emojiInput = document.querySelector('.emoji-input')
-  const links = document.querySelectorAll('.emoji-link')
-  links.forEach((item) => {
-    item.onclick = (event) => {
-      document.querySelector('.emoji-input').value = document.querySelector('.emoji-input').value + event.target.innerHTML
-    }
+
+  const emojiContainer = document.createElement('div')
+  emojiContainer.setAttribute('class', "emoji-container")
+
+  const emojiPicker = document.createElement('div')
+  emojiPicker.setAttribute('class', "emoji-picker")
+
+  const emojiList = document.createElement('ul')
+  const emojis = [0x1F601, 0x1F604, 0x1F60D];
+
+  const clickLink = (event) => {
+    document.querySelector('.emoji-input').value = document.querySelector('.emoji-input').value + event.target.innerHTML
+  }
+
+  emojis.map((item) => {
+      const emojiLi = document.createElement('li')
+      const emojiLink = document.createElement('a')
+      emojiLink.setAttribute('href', "#")
+      emojiLink.innerHTML = String.fromCodePoint(item)
+      emojiLink.onclick = clickLink
+
+      emojiList.appendChild(emojiLink)
   })
+
+  emojiPicker.appendChild(emojiList)
+  emojiContainer.appendChild(emojiPicker)
+
+  emojiInput.parentNode.insertBefore(emojiContainer, emojiInput.nextSibling)
+
+  console.log(emojiList)
 })()
