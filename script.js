8332a434094838e0173b4540f7d71978df3d4ec5
Glauber dutra ramos <glauberamos@gmail.com>
2017-07-15 19:25:27 -0300

    Inline css

--- script.js
+++ script.js
@@ -21,16 +21,31 @@ const getAllElementsWithAttribute = (attribute) => {
     }
 
     const emojiContainer = document.createElement('div')
-    emojiContainer.setAttribute('class', "emoji-container")
+    emojiContainer.style = `position: relative;
+      width: 300px;`
 
     parent.replaceChild(emojiContainer, emojiInput)
     emojiContainer.appendChild(emojiInput)
 
     const emojiPicker = document.createElement('div')
-    emojiPicker.setAttribute('class', "emoji-picker")
+    emojiPicker.style = `position: absolute;
+      right: 0;
+      z-index: 999;
+      display: none;
+      width: 225px;
+      padding: 5px 2px 5px 5px;
+      margin-top: 5px;
+      overflow: hidden;
+      background: #fff;
+      border: 1px #dfdfdf solid;
+      border-radius: 3px;
+      box-shadow: 0 1px 1px rgba(0, 0, 0, 0.1);`
 
     const emojiTrigger = document.createElement('a')
-    emojiTrigger.setAttribute('class', "emoji-trigger")
+    emojiTrigger.style = `position: absolute;
+      top: 0;
+      right: 0;
+      text-decoration: none;`
     emojiTrigger.setAttribute('href', "#")
     emojiTrigger.innerHTML = ":)"
     emojiTrigger.onclick = () => {
@@ -40,11 +55,17 @@ const getAllElementsWithAttribute = (attribute) => {
     emojiContainer.appendChild(emojiTrigger)
 
     const emojiList = document.createElement('ul')
+    emojiList.style = `padding: 0;
+      list-style: none;`
     const emojis = [0x1F601, 0x1F604, 0x1F60D];
 
     emojis.map((item) => {
         const emojiLi = document.createElement('li')
+        emojiLi.style = `display: inline-block;
+          margin: 5px;`
+
         const emojiLink = document.createElement('a')
+        emojiLink.style = `text-decoration: none;`
         emojiLink.setAttribute('href', "#")
         emojiLink.innerHTML = String.fromCodePoint(item)
         emojiLink.onclick = clickLink
